40900a75cc094b7e47f1e6c3837b838d10a0088a
Phelim Dunleavy <dunleavy.phelim@gmail.com>
2019-04-25 19:46:51 +0100

    add photo model with title description and image

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_25_162854) do
+ActiveRecord::Schema.define(version: 2019_04_25_183535) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "photos", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
