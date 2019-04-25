5c2a3dc12b1d559aa39996113e2cfe4c81b94e28
Phelim Dunleavy <dunleavy.phelim@gmail.com>
2019-04-25 20:00:02 +0100

    create basic photo controller with empty view for new photos

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,2 +1,25 @@
 class PhotosController < ApplicationController
+  before_action :authenticate_user!
+  def new
+    new_photo
+  end
+
+  def create
+    new_photo
+    if @photo.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def new_photo
+    @photo = Photo.new(photo_params)
+  end
+
+  def photo_params
+    params.require(:photo).permit(:title, :image, :description)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users, skip: :registrations, controllers: { registrations: 'registrations' }
 
   root to: 'pages#home'
+  resources :photos, only: %i[new create]
 end
